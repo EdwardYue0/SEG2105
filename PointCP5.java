3859d84c3b31c7a0a7e1153954b9639c6667eb69
Vicky-Feng <71288189+Vicky-Feng@users.noreply.github.com>
2020-10-04 11:50:43 -0400

    Add files via upload

---
+++ PointCP5.java
@@ -0,0 +1,18 @@
+public abstract class PointCP5{
+
+	//2 constructor
+
+	//desig2 extends design5
+	//desig3 extends design5
+	//new point();
+	
+
+
+	public abstract double getX();
+	public abstract double getY();
+	public abstract double getRho();
+	public abstract double getTheta();
+	public abstract void convertStorageToPolar();
+	public abstract void convertStorageToCartesian();
+
+}
