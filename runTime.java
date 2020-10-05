30fffc1482a736994920b1c337ff921f39f518fa
Vicky <yfeng045@uottawa.ca>
2020-10-04 22:20:49 -0400

    Update runTime.java

--- runTime.java
+++ runTime.java
@@ -3,36 +3,42 @@ public class runTime {
     /**
      * Find average time to initialize PointCP
      * @param type char value of either C or P
-     * @param testTimes time to run test in seconds
+     * @param testTimeSeconds time to run test in seconds
      * @return average operation time in nanoseconds
      */
-    public static long initPointCP(char type, long testTimes){
+    public static long initPointCP(char type, long testTimeSeconds){
+        long testTimeNano = testTimeSeconds * 1000 * 1000 * 1000;
         long count = 0;
-        long startTime = System.currentTimeMillis();
-        while(count<testTimes){
+        long startTime = System.nanoTime();
+        long endTime = startTime + testTimeNano;
+        while(System.nanoTime() < endTime){
             new PointCP(type, 10*Math.random(), 10*Math.random());
             count++;
         }
-        long endTime = System.currentTimeMillis();
-        long result = endTime-startTime;
+        long result = testTimeNano/count;
         return result;
     }
 
     /**
-     * Find average time to initialize PointCP Design 2
+     * Find average time to initialize PointCP Design 5
      * @param type char value of either C or P
-     * @param testTimes time to run test in seconds
+     * @param testTimeSeconds time to run test in seconds
      * @return average operation time in nanoseconds
      */
-    public static long initPointCP2(char type, long testTimes){
+    public static long initPointCP5(char type, long testTimeSeconds){
+        long testTimeNano = testTimeSeconds * 1000 * 1000 * 1000;
         long count = 0;
-        long startTime = System.currentTimeMillis();
-        while(count<testTimes){
-            new PointCP2(type, 10*Math.random(), 10*Math.random());
+        long startTime = System.nanoTime();
+        long endTime = startTime + testTimeNano;
+        while(System.nanoTime() < endTime){
+            if(type=='C'){
+                new PointCP2(type, 10*Math.random(), 10*Math.random());
+            }
+            if(type=='P'){
+                new PointCP3(type, 10*Math.random(), 10*Math.random());}
             count++;
         }
-        long endTime = System.currentTimeMillis();
-        long result = endTime-startTime;
+        long result = testTimeNano/count;
         return result;
     }
 
@@ -40,42 +46,50 @@ public class runTime {
     /**
      * Find average time to get each Cartesian coordinate from getter method in PointCP
      * @param type char value of either C or P
-     * @param testTimes time to run test in seconds
+     * @param testTimeSeconds time to run test in seconds
      * @return average operation time in nanoseconds
      */
-    public static long getCPointCP(char type, long testTimes){
+    public static long getCPointCP(char type, long testTimeSeconds){
+        long testTimeNano = testTimeSeconds * 1000 * 1000 * 1000;
         long count = 0;
-        long startTime = System.currentTimeMillis();
+        long startTime = System.nanoTime();
+        long endTime = startTime + testTimeNano;
         PointCP tmp = new PointCP(type, 10*Math.random(), 10*Math.random());
-        while(count<testTimes){
+        while(System.nanoTime() < endTime){
             tmp.getX();
             count++;
             tmp.getY();
             count++;
         }
-        long endTime = System.currentTimeMillis();
-        long result = endTime-startTime;
+        long result = testTimeNano/count;
         return result;
     }
 
     /**
-     * Find average time to get each Cartesian coordinate from getter method in PointCP Design 2
+     * Find average time to get each Cartesian coordinate from getter method in PointCP Design 5
      * @param type char value of either C or P
-     * @param testTimes time to run test in seconds
+     * @param testTimeSeconds time to run test in seconds
      * @return average operation time in nanoseconds
      */
-    public static long getCPointCP2(char type, long testTimes) {
+    public static long getCPointCP5(char type, long testTimeSeconds) {
+        long testTimeNano = testTimeSeconds * 1000 * 1000 * 1000;
         long count = 0;
-        long startTime = System.currentTimeMillis();
-        PointCP2 tmp = new PointCP2(type, 10 * Math.random(), 10 * Math.random());
-        while (count<testTimes) {
+        long startTime = System.nanoTime();
+        long endTime = startTime + testTimeNano;
+        PointCP5 tmp;
+        if(type=='C'){
+            tmp=new PointCP2(type, 10 * Math.random(), 10 * Math.random());
+        }
+        else{
+            tmp=new PointCP3(type, 10 * Math.random(), 10 * Math.random());
+        }
+        while (System.nanoTime() < endTime) {
             tmp.getX();
             count++;
             tmp.getY();
             count++;
         }
-        long endTime = System.currentTimeMillis();
-        long result = endTime-startTime;
+        long result = testTimeNano / count;
         return result;
     }
 
@@ -83,42 +97,50 @@ public class runTime {
     /**
      * Find average time to get each Polar  coordinate from getter method in PointCP
      * @param type char value of either C or P
-     * @param testTimes time to run test in seconds
+     * @param testTimeSeconds time to run test in seconds
      * @return average operation time in nanoseconds
      */
-    public static long getPPointCP(char type, long testTimes) {
+    public static long getPPointCP(char type, long testTimeSeconds) {
+        long testTimeNano = testTimeSeconds * 1000 * 1000 * 1000;
         long count = 0;
-        long startTime = System.currentTimeMillis();
+        long startTime = System.nanoTime();
+        long endTime = startTime + testTimeNano;
         PointCP tmp = new PointCP(type, 10 * Math.random(), 10 * Math.random());
-        while (count<testTimes) {
+        while (System.nanoTime() < endTime) {
             tmp.getRho();
             count++;
             tmp.getTheta();
             count++;
         }
-        long endTime = System.currentTimeMillis();
-        long result = endTime-startTime;
+        long result = testTimeNano / count;
         return result;
     }
 
     /**
-     * Find average time to get each Polar  coordinate from getter method in PointCP Design 2
+     * Find average time to get each Polar  coordinate from getter method in PointCP Design 5
      * @param type char value of either C or P
-     * @param testTimes time to run test in seconds
+     * @param testTimeSeconds time to run test in seconds
      * @return average operation time in nanoseconds
      */
-    public static long getPPointCP2(char type, long testTimes) {
+    public static long getPPointCP5(char type, long testTimeSeconds) {
+        long testTimeNano = testTimeSeconds * 1000 * 1000 * 1000;
         long count = 0;
-        long startTime = System.currentTimeMillis();
-        PointCP2 tmp = new PointCP2(type, 10 * Math.random(), 10 * Math.random());
-        while (count<testTimes) {
+        long startTime = System.nanoTime();
+        long endTime = startTime + testTimeNano;
+        PointCP5 tmp;
+        if(type=='C'){
+            tmp=new PointCP2(type, 10 * Math.random(), 10 * Math.random());
+        }
+        else{
+            tmp=new PointCP3(type, 10 * Math.random(), 10 * Math.random());
+        }
+        while (System.nanoTime() < endTime) {
             tmp.getRho();
             count++;
             tmp.getTheta();
             count++;
         }
-        long endTime = System.currentTimeMillis();
-        long result = endTime-startTime;
+        long result = testTimeNano / count;
         return result;
     }
 
@@ -129,20 +151,20 @@ public class runTime {
      * @param args
      */
     public static void main(String[] args){
-        int testTimes = 100000000;
-        System.out.println("Elapsed time for initializing C PointCP: "+initPointCP('C', testTimes)+"ms");
-        System.out.println("Elapsed time for initializing C PointCP2: "+initPointCP2('C', testTimes)+"ms");
-        System.out.println("Elapsed time for initializing P PointCP: "+initPointCP('P', testTimes)+"ms");
-        System.out.println("Elapsed time for initializing P PointCP2: "+initPointCP2('P', testTimes)+"ms");
+        int testTime = 3;
+        System.out.println("Average time for initializing C PointCP: "+initPointCP('C', testTime)+"ns");
+        System.out.println("Average time for initializing C PointCP5: "+initPointCP5('C', testTime)+"ns");
+        System.out.println("Average time for initializing P PointCP: "+initPointCP('P', testTime)+"ns");
+        System.out.println("Average time for initializing P PointCP5: "+initPointCP5('P', testTime)+"ns");
 
-        System.out.println("Elapsed time for getting Cartesian from Cartesian PointCP: "+getCPointCP('C', testTimes)+"ms");
-        System.out.println("Elapsed time for getting Cartesian from Cartesian PointCP2: "+getCPointCP2('C', testTimes)+"ms");
-        System.out.println("Elapsed time for getting Polar from Cartesian PointCP: "+getPPointCP('C', testTimes)+"ms");
-        System.out.println("Elapsed time for getting Polar from Cartesian PointCP2: "+getPPointCP2('C', testTimes)+"ms");
+        System.out.println("Average time for getting Cartesian when stroing Cartesian PointCP: "+getCPointCP('C', testTime)+"ns");
+        System.out.println("Average time for getting Cartesian when stroing Cartesian PointCP5: "+getCPointCP5('C', testTime)+"ns");
+        System.out.println("Average time for getting Polar when stroing Cartesian PointCP: "+getPPointCP('C', testTime)+"ns");
+        System.out.println("Average time for getting Polar when stroing Cartesian PointCP5: "+getPPointCP5('C', testTime)+"ns");
 
-        System.out.println("Elapsed time for getting Cartesian from Polar PointCP: "+getCPointCP('P', testTimes)+"ms");
-        System.out.println("Elapsed time for getting Cartesian from Polar PointCP2: "+getCPointCP2('P', testTimes)+"ms");
-        System.out.println("Elapsed time for getting Polar from Polar PointCP: "+getPPointCP('P', testTimes)+"ms");
-        System.out.println("Elapsed time for getting Polar from Polar PointCP2: "+getPPointCP2('P', testTimes)+"ms");
+        System.out.println("Average time for getting Cartesian when stroing Polar PointCP: "+getCPointCP('P', testTime)+"ns");
+        System.out.println("Average time for getting Cartesian when stroing Polar PointCP5: "+getCPointCP5('P', testTime)+"ns");
+        System.out.println("Average time for getting Polar when stroing Polar PointCP: "+getPPointCP('P', testTime)+"ns");
+        System.out.println("Average time for getting Polar when stroing Polar PointCP5: "+getPPointCP5('P', testTime)+"ns");
     }
 }
